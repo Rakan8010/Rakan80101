b00e1cf64433f5d8cd42d0684ab4d78a2e845515
Rakan8010 <naver88552@gmail.com>
2022-08-26 12:20:04 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import infra.Application;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Application app = new Application("naver");
+        app.run();
+
+    }
+}
---
+++ src/service/MemberService.java
@@ -0,0 +1,38 @@
+package service;
+
+import bata.Member;
+import infra.Container;
+import repository.MemberRepository;
+
+public class MemberService {
+
+    private MemberRepository memberRepository;
+
+    public MemberService(){
+        this.memberRepository = Container.memberRepository;
+    }
+
+    public int saveMember(String loginId, String password, String name){
+        return memberRepository.saveMember(loginId, password, name);
+    }
+
+    public boolean isExistsByLoginId(String loginId){
+        return memberRepository.isExistsByLoginId(loginId);
+    }
+
+    public boolean isCorrectInfo(String loginId, String password){
+
+        Member findMember = memberRepository.getMemberByLoginId(loginId);
+
+        if(findMember == null){
+            return false;
+        }
+
+        if(findMember.getPassword().equals(password)){
+            return true;
+        }
+        return false;
+    }
+
+
+}
